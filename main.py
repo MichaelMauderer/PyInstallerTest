926701198fb737b8bd1158ed22b9c7b586e09dd3
Michael Mauderer <mail@michaelmauderer.com>
2016-04-13 15:53:45 +0100

    Add numpy dependency.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from __future__ import unicode_literals, division, print_function
 
 import argparse
+import numpy
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', action='store_true',
                         help='run test suite')
     args = parser.parse_args()
-
+    print(numpy.__version__)
     print("HelloWorld")
