1b80e9a6436d479ad7ad1561ccd996fee50cb027
Michael Mauderer <mail@michaelmauderer.com>
2016-04-13 15:42:26 +0100

    Add parameter passing.

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from __future__ import unicode_literals, division, print_function
 
+import argparse
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', action='store_true',
+                        help='run test suite')
+    args = parser.parse_args()
+
     print("HelloWorld")
