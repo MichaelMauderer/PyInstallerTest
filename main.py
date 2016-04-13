fbbe8e40caa64011875a4eb27ed5ceffda85cdcd
Michael Mauderer <mail@michaelmauderer.com>
2016-04-13 16:18:58 +0100

    Test nose testrunner.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from __future__ import unicode_literals, division, print_function
 
 import argparse
-import numpy
+import nose
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', action='store_true',
                         help='run test suite')
     args = parser.parse_args()
-    print(numpy.__version__)
+    nose.main()
     print("HelloWorld")
