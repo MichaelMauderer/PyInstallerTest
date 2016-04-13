bd1a129a5e6be44b5c4b5648b70acfedab15e628
Michael Mauderer <mail@michaelmauderer.com>
2016-04-13 21:37:27 +0100

    Add failing test.

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from __future__ import unicode_literals, division, print_function
 import argparse
 import nose
 import sys
+import test
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', action='store_true',
                         help='run test suite')
     args = parser.parse_args()
-    nose.main(argv=sys.argv[:1], module ='test')
+    nose.main(argv=sys.argv[:1], suite=[test.test_fail])
     print("HelloWorld")
