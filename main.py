f5b00fe32ec2911aed2acae56a0a6f12c985967c
Michael Mauderer <mail@michaelmauderer.com>
2016-04-13 17:20:06 +0100

    Pass argv parameters to nose.

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from __future__ import unicode_literals, division, print_function
 
 import argparse
 import nose
+import sys
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', action='store_true',
                         help='run test suite')
     args = parser.parse_args()
-    nose.main()
+    nose.main(argv=sys.argv[:1])
     print("HelloWorld")
